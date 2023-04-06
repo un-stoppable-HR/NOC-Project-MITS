4adbd0cf13f51bb091aaf978949e61fe0390581a
Harsh Rajput <iamharshrajput@gmail.com>
2023-04-06 17:20:01 +0530

    Added User-Authentication & User-Authorization

--- data/database.js
+++ data/database.js
@@ -2,9 +2,10 @@ const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
   host: "localhost",
+  port: 3306,
   database: "noc",
   user: "root",
-  password: "",
+  password: "#Password@0101",
 });
 
 module.exports = pool;
