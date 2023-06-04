f0a20a8b18df1bf33d135473e61247ca86cebebe
Harsh Rajput <iamharshrajput@gmail.com>
2023-06-04 18:52:15 +0530

    Created Backend for T&P - Admin Panel

--- routes/tnp-routes.js
+++ routes/tnp-routes.js
@@ -16,7 +16,7 @@ router.get("/dashboard-tnp", function (req, res) {
   res.render("dashboard-tnp");
 });
 
-router.get("/admin-panel-tnp", function (req, res) {
+router.get("/admin-panel-tnp", async function (req, res) {
   if (!res.locals.isAuth) {
     return res.status(401).render("401");
   }
@@ -25,7 +25,56 @@ router.get("/admin-panel-tnp", function (req, res) {
     return res.status(403).render("403");
   }
 
-  res.render("admin-panel-tnp");
+  const query1 = `
+  SELECT 
+    users.user_id, 
+    users.email, 
+    COALESCE(department_coordinator.department_id, NULL) AS department_id
+  FROM users
+  LEFT JOIN department_coordinator
+  ON users.user_id = department_coordinator.user_id
+  WHERE users.role = "department";
+  `;
+
+  const [potentialCoordinators] = await db.query(query1);
+  
+  res.render("admin-panel-tnp", {
+    potentialCoordinators: potentialCoordinators,
+  });
+});
+
+router.post("/admin-panel-tnp/:userID", async function(req, res) {
+  const userID = req.params.userID;
+  const departmentID = req.body.department;
+  
+  const query1 = `
+  SELECT *
+  FROM department_coordinator
+  WHERE user_id = ?
+  `;
+  
+  const [existingCoordinator] = await db.query(query1, userID);
+  
+  if (!existingCoordinator[0]) {
+    const coordinatorData1 = [userID, departmentID];
+    
+    const query2 = `
+    INSERT INTO department_coordinator (user_id, department_id) VALUES (?)
+    `;
+    
+    await db.query(query2, [coordinatorData1]);
+  }
+  
+  else {
+    const query3 = `
+    UPDATE department_coordinator SET department_id = ? 
+    WHERE user_id = ?
+    `;
+
+    await db.query(query3, [departmentID, userID]);
+  }
+
+  res.redirect("/admin-panel-tnp");
 });
 
 module.exports = router;
