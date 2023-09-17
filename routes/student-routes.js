ab05f229978ab0ee558f0cee48af57db5e29e1c7
Harsh Rajput <iamharshrajput@gmail.com>
2023-09-17 21:32:00 +0530

    Completed Student Dashboard

--- routes/student-routes.js
+++ routes/student-routes.js
@@ -255,4 +255,93 @@ router.post(
   }
 );
 
+router.post("/dashboard-student/:nocID", async function (req, res) {
+  if (!res.locals.isAuth) {
+    return res.status(401).render("401");
+  }
+
+  if (res.locals.role !== "student") {
+    return res.status(403).render("403");
+  }
+
+  const nocID = req.params.nocID;
+
+  const query1 = `
+  SELECT 
+    noc_applications.noc_id,
+    noc_applications.enrollment_no,
+    noc_applications.internship_days,
+    noc_applications.internship_start_date,
+    noc_applications.internship_end_date,
+    noc_applications.applying_through,
+    noc_applications.date,
+    noc_applications.stipend,
+    students.user_id,
+    students.name AS student_name,
+    students.mobile_no AS student_mobile_no,
+    students.father_name,
+    students.semester,
+    students.current_address AS student_current_address,
+    students.city AS student_city,
+    students.state AS student_state,
+    students.pin_code AS student_pin_code,
+    students.branch_id,
+    students.department_id,
+    organizations.name AS organization_name,
+    organizations.location AS organization_location,
+    organizations.person_receiving_noc_name,
+    organizations.person_receiving_noc_designation,
+    organizations.address AS organization_address,
+    organizations.website AS organization_website,
+    department_approval.approval_status AS department_status,
+    tpo_approval.approval_status AS tpo_status
+  FROM 
+    noc_applications
+    INNER JOIN organizations 
+      ON noc_applications.org_id = organizations.org_id
+    INNER JOIN department_approval
+      ON noc_applications.noc_id = department_approval.noc_id
+    INNER JOIN tpo_approval
+      ON noc_applications.noc_id = tpo_approval.noc_id
+    INNER JOIN students
+      ON noc_applications.enrollment_no = students.enrollment_no
+  WHERE noc_applications.noc_id = ?;
+  `;
+
+  const [nocDetails] = await db.query(query1, nocID);
+
+  const branchID = nocDetails[0].branch_id;
+  const departmentID = nocDetails[0].department_id;
+  const userID = nocDetails[0].user_id;
+
+  const query2 = `
+  SELECT branch_name FROM branches WHERE branch_id = ?;
+  `;
+
+  const [branchName] = await db.query(query2, branchID);
+
+  const query3 = `
+  SELECT department_name FROM departments WHERE department_id = ?;
+  `;
+
+  const [departmentName] = await db.query(query3, departmentID);
+
+  const query4 = `
+  SELECT email FROM users WHERE user_id = ?;
+  `;
+
+  const [studentEmail] = await db.query(query4, userID);
+
+  req.session.moreDetails = {
+    ...nocDetails[0],
+    ...branchName[0],
+    ...departmentName[0],
+    ...studentEmail[0],
+  };
+
+  req.session.save(function () {
+    res.redirect("/more-details");
+  });
+});
+
 module.exports = router;
