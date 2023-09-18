fd045b0fa4939ad728e4f0bef153507f4ecc9cf3
Harsh Rajput <iamharshrajput@gmail.com>
2023-09-18 12:16:04 +0530

    Created Backend for Dynamic NOC

--- routes/base-routes.js
+++ routes/base-routes.js
@@ -74,7 +74,7 @@ router.get("/more-details", function (req, res) {
   req.session.moreDetails = null;
 
   req.session.save(function () {
-    res.render("more-details", { moreDetails: moreDetails});
+    res.render("more-details", { moreDetails: moreDetails });
   });
 });
 
@@ -230,7 +230,90 @@ router.post("/logout", function (req, res) {
 });
 
 router.get("/noc", function (req, res) {
-  res.render("noc");
+  if (!res.locals.isAuth) {
+    return res.status(401).render("401");
+  }
+
+  const nocDetails = req.session.nocDetails;
+
+  if (
+    !nocDetails ||
+    (nocDetails.department_status != "approved" &&
+    nocDetails.tpo_status != "approved")
+  ) {
+    return res.redirect("/");
+  }
+
+  req.session.nocDetails = null;
+
+  req.session.save(function () {
+    res.render("noc", { nocDetails: nocDetails });
+  });
+});
+
+router.post("/download-noc/:nocID", async function (req, res) {
+  if (!res.locals.isAuth) {
+    return res.status(401).render("401");
+  }
+
+  const nocID = req.params.nocID;
+
+  const query1 = `
+  SELECT 
+    noc_applications.noc_id,
+    noc_applications.enrollment_no,
+    noc_applications.internship_start_date,
+    noc_applications.internship_end_date,
+    students.name AS student_name,
+    students.branch_id,
+    students.department_id,
+    organizations.name AS organization_name,
+    organizations.location AS organization_location,
+    organizations.person_receiving_noc_name,
+    organizations.person_receiving_noc_designation,
+    organizations.address AS organization_address,
+    department_approval.approval_status AS department_status,
+    tpo_approval.approval_status AS tpo_status,
+    tpo_approval.approval_date AS tpo_approval_date
+  FROM 
+    noc_applications
+    INNER JOIN organizations 
+      ON noc_applications.org_id = organizations.org_id
+    INNER JOIN department_approval
+      ON noc_applications.noc_id = department_approval.noc_id
+    INNER JOIN tpo_approval
+      ON noc_applications.noc_id = tpo_approval.noc_id
+    INNER JOIN students
+      ON noc_applications.enrollment_no = students.enrollment_no
+  WHERE noc_applications.noc_id = ?;
+  `;
+
+  const [nocDetails] = await db.query(query1, nocID);
+
+  const branchID = nocDetails[0].branch_id;
+  const departmentID = nocDetails[0].department_id;
+
+  const query2 = `
+  SELECT branch_name FROM branches WHERE branch_id = ?;
+  `;
+
+  const [branchName] = await db.query(query2, branchID);
+
+  const query3 = `
+  SELECT department_name FROM departments WHERE department_id = ?;
+  `;
+
+  const [departmentName] = await db.query(query3, departmentID);
+
+  req.session.nocDetails = {
+    ...nocDetails[0],
+    ...branchName[0],
+    ...departmentName[0],
+  };
+
+  req.session.save(function () {
+    res.redirect("/noc");
+  });
 });
 
 module.exports = router;
