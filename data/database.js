84c7aac788e15dfe6bcaa4bad90d0756139ce961
Harsh Rajput <iamharshrajput@gmail.com>
2023-11-18 14:00:48 +0530

    Added Database Password in .env

--- data/database.js
+++ data/database.js
@@ -1,11 +1,13 @@
 const mysql = require("mysql2/promise");
 
+require("dotenv").config();
+
 const pool = mysql.createPool({
   host: "localhost",
   port: 3306,
   database: "noc",
   user: "root",
-  password: "#Password@0101",
+  password: process.env.DB_PASSWORD,
 });
 
 module.exports = pool;
